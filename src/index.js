93658f90ff6d057f68090c109bcc1cbef1588e15
JooGo01 <joseakd136@gmail.com>
2021-08-24 20:02:44 -0300

    Update 3.0

--- src/index.js
+++ src/index.js
@@ -11,20 +11,38 @@ app.set('port', 3000);
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(cors());
 
 //static files
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var whiteList =['http://localhost:3000/']
+
+var corsOptions =
+{
+	origin: function(origin, callback)
+	{
+		if(whiteList.indexOf(origin)!= -1)
+		{
+			callback(null, true)
+		}
+		else
+		{
+			callback(new Error('Not allowed by CORS'));
+		}
+	}
+}
+
 //getting video data--download
 
-app.get("/videoInfo",async function(request,response){
+app.get("/videoInfo", cors(corsOptions), async function(request,response){
 	const videoURL = request.query.videoURL;
 	const info = await ytdl.getInfo(videoURL);
 	response.status(200).json(info);
 });
 
-app.get("/download",function(request,response){
+app.get("/download", function(request,response){
 	const videoURL = request.query.videoURL;
 	const itag = request.query.itag;
 	const format = request.query.format;
--- src/public/function/indexFunction.js
+++ src/public/function/indexFunction.js
@@ -9,68 +9,57 @@ function getVideoInfo(){
     return;
   }
 
-  fetch(host+"videoInfo?videoURL=" + videoURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      let detailsNodes = {
+  fetch(host + "videoInfo?videoURL=" + videoURL, {mode: 'no-cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let detailsNodes = {
         thumbnail: document.querySelector(".video-data .thumbnail img"),
         title: document.querySelector(".video-data .info h2"),
         description: document.querySelector(".video-data .info p"),
         videoURL: document.querySelector(".video-data .controls #video-URL-OPTIONS"),
-        downloadOptions: document.querySelector(
-          ".video-data .controls #download-options"
-        ),
-      };
+        downloadOptions: document.querySelector(".video-data .controls #download-options")
+    }
 
-      let html = "";
-      let repeat=false;
-      for (let i = 0; i < data.formats.length; i++) {
-        
+    let html = "";
+    let repeat = false;
+    for (let i = 0; i < data.formats.length; i++) {
+
         if (data.formats[i].container != "mp4") {
-          continue;
-        }
-        
-        if(data.formats[i].qualityLabel == null)
-        {
-          continue;
-        }
-        else(data.formats[i].qualityLabel!=null && i>0)
-        {
-          for(let ii=0; ii<i; ii++)
-          {
-            if(data.formats[i].qualityLabel == data.formats[ii].qualityLabel)
-            {
-              repeat=true;
-            }
-          }
-          if(repeat==true)
-          {
-            repeat=false;
             continue;
-          }
+        }
+
+        if (data.formats[i].qualityLabel == null) {
+            continue;
+        } 
+        else(data.formats[i].qualityLabel != null && i > 0) 
+        {
+            for (let ii = 0; ii < i; ii++) {
+                if (data.formats[i].qualityLabel == data.formats[ii].qualityLabel) {
+                    repeat = true;
+                }
+            }
+            if (repeat == true) {
+                repeat = false;
+                continue;
+            }
         }
         html += `<option value="${data.formats[i].itag}|${data.formats[i].container}">
-          ${data.formats[i].container} - ${data.formats[i].qualityLabel}</option>`;
+      ${data.formats[i].container} - ${data.formats[i].qualityLabel}</option>`;
 
-        detailsNodes.thumbnail.src =
-        data.videoDetails.thumbnails[data.videoDetails.thumbnails.length - 1].url; // get img
+        detailsNodes.thumbnail.src = data.videoDetails.thumbnails[data.videoDetails.thumbnails.length - 1].url; // get img
         detailsNodes.title.innerText = data.videoDetails.title;
         detailsNodes.description.innerText = data.videoDetails.description;
         detailsNodes.videoURL.value = videoURL;
         detailsNodes.downloadOptions.innerHTML = html;
 
         document.querySelector(".video-data").style.display = "block";
-        document.querySelector(".video-data").scrollIntoView({
-          behavior: "smooth",
-        });
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        document.querySelector(".video-data").scrollIntoView({behavior: "smooth"});
+    }
+  })
+  .catch(function(error){console.log(error);});
 };
 
 // ADDING EVENT TO THE BUTTON
